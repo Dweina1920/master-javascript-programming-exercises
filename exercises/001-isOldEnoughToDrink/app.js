d086b306e60370ac2d25314b1482df728a2655b9
Dina <dina.yamanid@gmail.com>
2023-05-16 09:42:46 +0200

    ex 2

--- exercises/001-isOldEnoughToDrink/app.js
+++ exercises/001-isOldEnoughToDrink/app.js
@@ -1,11 +1,10 @@
-function isOldEnoughToDrink(age){
-    // your code here	
-    if(age>=21){
-return true
-    }else{
-        return false 
-    }
-    
+function isOldEnoughToDrink(age) {
+  // your code here
+  if (age >= 21) {
+    return true;
+  } else {
+    return false;
+  }
 }
-console.log(isOldEnoughToDrink(20))
-console.log(isOldEnoughToDrink(22))
+console.log(isOldEnoughToDrink(20));
+console.log(isOldEnoughToDrink(22));
