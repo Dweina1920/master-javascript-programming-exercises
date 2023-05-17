1bbdcd0ba6ac7b0d6774336a0941037d247f7be7
Dina <dina.yamanid@gmail.com>
2023-05-17 12:11:59 +0200

    ex 7

--- exercises/007-removeProperty/app.js
+++ exercises/007-removeProperty/app.js
@@ -1,3 +1,15 @@
 function removeProperty(obj, key) {
   // your code here
+  delete obj[key]
 }
+
+
+let obj = {
+  name: 'Sam',
+  age: 20
+}
+removeProperty(obj, 'name');
+console.log(obj.name); // --> undefined
+removeProperty(obj, 'age');
+console.log(obj.age);
+console.log(obj)
