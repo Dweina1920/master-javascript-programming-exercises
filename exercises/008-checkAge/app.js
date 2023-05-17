12a5aa2630af5f8a4566261c45d23edd09d5d249
Dina <dina.yamanid@gmail.com>
2023-05-17 12:19:09 +0200

    ex 8

--- exercises/008-checkAge/app.js
+++ exercises/008-checkAge/app.js
@@ -1,3 +1,11 @@
 function checkAge(name, age) {
   // your code here
+  if (age>=21){
+    return `Welcome ${name}.`
+  }else{
+    return `${name} go home.`
+  }
 }
+
+console.log(checkAge("dina", 29))
+console.log(checkAge("dina", 20))
