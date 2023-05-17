21276f92a755ba864dbbc2875b2be11f1aeb0ae6
Dina <dina.yamanid@gmail.com>
2023-05-17 10:03:36 +0200

    ex 4

--- exercises/003-isOldEnoughToVote/app.js
+++ exercises/003-isOldEnoughToVote/app.js
@@ -1,3 +1,10 @@
 function isOldEnoughToVote(age) {
   // your code here
+  if (age>=20){
+    return true
+  }else{
+    return false
+  }
 }
+console.log( isOldEnoughToVote(19))
+console.log( isOldEnoughToVote(20))
