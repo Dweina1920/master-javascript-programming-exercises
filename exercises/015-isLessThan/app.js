34acfe85518f010b8402575a79327f0c4ca0d152
Dina <dina.yamanid@gmail.com>
2023-08-17 15:08:38 +0200

    ex 15 done

--- exercises/015-isLessThan/app.js
+++ exercises/015-isLessThan/app.js
@@ -1,3 +1,9 @@
 function isLessThan(num1, num2) {
   // your code here
+  if (num1 < num2) {
+    return `${num1} is less than ${num2}`;
+  }
+  return `${num2} is less than ${num1}`;
 }
+console.log(isLessThan(1, 4));
+console.log(isLessThan(4, 1));
