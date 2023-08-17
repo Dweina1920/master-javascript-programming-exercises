0b355166053918ff9fd1febe4a0a80f5e3193e2f
Dina <dina.yamanid@gmail.com>
2023-08-17 15:12:08 +0200

    ex 16 done

--- exercises/016-isGreaterThan/app.js
+++ exercises/016-isGreaterThan/app.js
@@ -1,3 +1,9 @@
 function isGreaterThan(num1, num2) {
   // your code here
+  if (num1> num2){
+   return `${num1} is greater than ${num2}`
+  }
+  return `${num2} is greater than ${num1}`
 }
+console.log(isGreaterThan(1,4))
+console.log(isGreaterThan(4,1))
