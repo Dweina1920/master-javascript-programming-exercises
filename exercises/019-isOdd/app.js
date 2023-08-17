e7ae184b5901425947fc2cd9350fe227deb8d013
Dina <dina.yamanid@gmail.com>
2023-08-17 15:23:19 +0200

    ex 19 done

--- exercises/019-isOdd/app.js
+++ exercises/019-isOdd/app.js
@@ -1,1 +1,9 @@
 // Write your function here
+
+// Write your function here
+function isOdd(num) {
+    return num % 2 == 1 ? true : false;
+  }
+  console.log(isOdd(5));
+  console.log(isOdd(4));
+  
