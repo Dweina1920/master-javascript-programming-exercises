f25a96962e2b0fe8384e1a0496bb7f1df519d937
Dina <dina.yamanid@gmail.com>
2023-08-17 15:27:04 +0200

    ex 20 done

--- exercises/020-isSameLength/app.js
+++ exercises/020-isSameLength/app.js
@@ -1,1 +1,6 @@
-// Write your function here
+// Write your function here// Write your function here
+function isSameLength(word1, word2) {
+  return word1.length == word2.length ? true : false;
+}
+console.log(isSameLength("hola", "hola"));
+console.log(isSameLength("hola", "holhfosha"));
